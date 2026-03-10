3cff5a6000d19ba2eb3613c36af019b69a9251eb
raulfrandes <frandesraul@gmail.com>
2026-03-10 17:19:55 +0200

    Set up project

---
+++ src/main/java/com/nevorent/backend/NevorentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nevorent.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NevorentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NevorentBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nevorent/backend/NevorentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nevorent.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NevorentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
